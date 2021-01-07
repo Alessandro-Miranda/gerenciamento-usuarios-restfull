22dc734c0aef0afad31f3e1ed335cac8d1540851
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-07 15:20:23 -0300

    Projeto finalizado

--- restapi/routes/index.js
+++ restapi/routes/index.js
@@ -1,6 +1,6 @@
 // Criação da rota utilizando o consign
 module.exports = app => {
-    app.get('/', (req, res) => {
+    app.get('/', (_req, res) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", 'text/html; charset=utf-8');
         res.end("<h1>Olá, mundo!</h1>");
